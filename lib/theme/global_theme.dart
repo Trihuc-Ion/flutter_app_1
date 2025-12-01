76bc5b707983c095c421cc8faf129e7356115ee2
Trihuc-Ion <onicaion03@gmail.com>
2025-12-01 12:39:23 +0200

    Update: apps global_theme

--- lib/theme/global_theme.dart
+++ lib/theme/global_theme.dart
@@ -1,46 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/resources/app_colors.dart';
 
 ThemeData lightTheme(){
   return ThemeData(
-    brightness: Brightness.light,
+    // brightness: Brightness.light,
     textTheme: TextTheme(
-      displayLarge: TextStyle(
-        color: Color.fromRGBO(26, 26, 26, 1),
+      headlineLarge: TextStyle(
         fontSize: 26,
+        color: AppColors.regularTitleColor,
+        fontWeight: FontWeight.w600,
+        height: 1,
+      ),
+
+      headlineMedium: TextStyle(
+        fontSize: 24,
+        color: AppColors.regularTitleColor,
         fontWeight: FontWeight.w600,
         height: 1
       ),
-      displayMedium: TextStyle(
-        color: Color.fromRGBO(26, 26, 26, 1),
+
+      headlineSmall: TextStyle(
         fontSize: 20,
+        color: AppColors.regularTitleColor,
         fontWeight: FontWeight.w500,
         height: 1
       ),
-      displaySmall: TextStyle(
-        color: Color.fromRGBO(26, 26, 26, 1),
+
+      titleLarge: TextStyle(
+        fontSize: 20,
+        color: AppColors.regularTitleColor,
+        fontWeight: FontWeight.w700,
+        height: 28 / 20
+      ),
+
+      titleMedium: TextStyle(
         fontSize: 18,
+        color: AppColors.regularTitleColor,
         fontWeight: FontWeight.w700,
         height: 24 / 18
       ),
-      bodyLarge: TextStyle(
+
+      displayLarge: TextStyle(
         fontFamily: 'SourceSans3',
-        color: Color.fromRGBO(153, 153, 153, 1),
         fontSize: 18,
+        color: AppColors.regularInfoColor,
         fontWeight: FontWeight.w400,
-        height: 1
+        height: 1,
+        letterSpacing: 0
       ),
+
+      displayMedium: TextStyle(
+        fontFamily: 'SourceSans3',
+        fontSize: 16,
+        color: AppColors.regularInfoColor,
+        fontWeight: FontWeight.w400,
+        height: 1,
+        letterSpacing: 0
+      ),
+
       bodyMedium: TextStyle(
         fontFamily: 'SourceSans3',
-        color: Color.fromRGBO(153, 153, 153, 1),
         fontSize: 16,
+        color: AppColors.regularContentColor,
         fontWeight: FontWeight.w400,
+        height: 26 / 16,
+        letterSpacing: 0
+      ),
+
+      labelLarge: TextStyle(
+        fontSize: 16,
+        color: AppColors.followColor,
+        fontWeight: FontWeight.w500,
         height: 1
       ),
-    ),
-      // primaryColor: ,
-      // colorScheme: ColorScheme.light(
 
-      // )
+      labelMedium: TextStyle(
+        fontFamily: 'SourceSans3',
+        fontSize: 14,
+        color: AppColors.regularBoxCategoryColor,
+        fontWeight: FontWeight.w600,
+        height: 1,
+        letterSpacing: 0
+      ),
+
+      labelSmall: TextStyle(
+        fontFamily: 'SourceSans3',
+        fontSize: 12,
+        color: AppColors.regularCategoryColor,
+        fontWeight: FontWeight.w600,
+        height: 1,
+        letterSpacing: 0
+      ),
+    ),
   );
 }
-  
